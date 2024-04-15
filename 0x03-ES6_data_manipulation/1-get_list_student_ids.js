908031eadca8daded25bea29e1a2a20fdb0ff0a8
Mafika <rudolphmafika@gmail.com>
2024-04-15 20:10:38 +0000

    task1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(studentList) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  const studentIds = studentList.map((student) => student.id);
+  return studentIds;
+}
