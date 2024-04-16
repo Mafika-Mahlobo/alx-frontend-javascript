ab4784a083b18422fe1185e96b70b7fb3bc476f6
Mafika <rudolphmafika@gmail.com>
2024-04-16 22:49:59 +0000

    edited task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '') {
-    return;
+    return '';
   }
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
   const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
