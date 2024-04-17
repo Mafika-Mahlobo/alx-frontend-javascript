a01f59563ab4ed511eccb41d88d6f8af2e0feb54
Mafika <rudolphmafika@gmail.com>
2024-04-17 14:01:52 +0000

    initial commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,39 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+   firstName: "john",
+   lastName: "doe",
+   age: 25,
+   location: "location1",
+};
+
+const student2: Student = {
+  firstName: "jane",
+  lastName: "doe",
+  age: 20,
+  location: "location2",
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const renderTable = ((students: Student[]) => {
+  const table = document.createElemnt('table');
+
+  students.forEach((student) => {
+    const row = table.insertRow();
+    const col1 = row.insertCell();
+    const col2 = row.insertCell();
+    col1.textContent = student.firstName;
+    col2.textContent = student.location;
+  });
+
+  document.body.appendChild(table);
+
+};
+
+rendeTable(studentsList);
